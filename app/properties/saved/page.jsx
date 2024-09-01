3184107900257a4039fe6fdfab5e3170593649a5
Shahroz Ahmed <shahroz.khan2124238@gmail.com>
2024-09-01 21:25:25 +0500

    Fixed loading UI for properties/[id]. Made home skeleton using React suspense

--- app/properties/saved/page.jsx
+++ app/properties/saved/page.jsx
@@ -10,7 +10,6 @@ const SavedPropertiesPage = async () => {
 
     const { userId } = sessionUser;
 
-    // NOTE: here we can make one database query by using Model.populate
     const { bookmarks } = await User.findById(userId)
         .populate('bookmarks')
         .lean();
